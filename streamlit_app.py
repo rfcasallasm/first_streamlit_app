a2eea964fd0c1ae0e39084e58f13588a5941ba6a
Raul Fernando Casallas Malaver <rfcasallasm@gmail.com>
2023-04-19 13:49:03 -0500

    Update streamlit_app.py
    adding second header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,16 @@
 import streamlit
 
 streamlit.title("My parents New Healthy Dinner")
+
 streamlit.header('Breakfast Menu')
+
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
 
 
    
