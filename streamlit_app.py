d5e2c1db525cb8fa95e3e57f60f95b2d0895e9a2
Raul Fernando Casallas Malaver <rfcasallasm@gmail.com>
2023-04-20 17:21:47 -0500

    Update streamlit_app.py
    closing connections, fixing title

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,9 +43,10 @@ def get_fruit_load_list():
       my_cur.execute("select * from fruit_load_list")
       return my_cur.fetchall()
   
-if streamlit.button('Get Fruit Load List'):
+if streamlit.button('Get Fruit Load List - Add Your Favorites!'):
    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
    my_data_rows = get_fruit_load_list()
+   my_cnx.close()
    streamlit.dataframe(my_data_rows)
 
 def insert_row_snowflake(new_fruit):
@@ -57,4 +58,5 @@ add_my_fruit = streamlit.text_input('What fruit would you like to add?','')
 if streamlit.button('Add a Fruit to the List'):
    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
    streamlit.text(insert_row_snowflake(add_my_fruit))
+   my_cnx.close()
    
